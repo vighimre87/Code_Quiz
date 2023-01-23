ed21dbd1007ed0e693300d4578b96f50d12f639f
Imre Vigh <vighimre87@gmail.com>
2023-01-23 16:24:39 +0000

    created questions for the quiz

---
+++ assets/js/questions.js
@@ -0,0 +1,30 @@
+// 
+
+const questions = [
+    {
+        question: "Javascript is an ___________ language?",
+        answers: ["Object-oriented", "Object-based", "Procedural", "None of the above"],
+        correct: "Object-oriented",
+    },
+    {
+        question: "Which of the following keywords is used to define a variable in Javascript?",
+        answers: ["var", "let", "Both A and B", "None of the above"],
+        correct: "Both A and B",
+    },
+    {
+        question: "Which of the following methods is used to access HTML elements using Javascript?",
+        answers: ["getElementById()", "getElementsByClassName()", "Both A and B", "None of the above"],
+        correct: "Both A and B",
+    },
+    {
+        question: "When an operator’s value is NULL, the typeof returned by the unary operator is:",
+        answers: ["Boolean", "Undefined", "Object", "Integer"],
+        correct: "Object",
+    },
+    {
+        question: "How can a datatype be declared to be a constant type?",
+        answers: ["const", "var", "let", "constant"],
+        correct: "const"
+    }
+
+]
